5c5100326a150aa5d7e6edb100c330cd28c65e58
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-07-27 13:23:30 +0300

    Test task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createItem(array) {
+    return array.reduce((acc, item) => acc + `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" 
+    src="${item.preview}" 
+    alt="${item.description}" />
+    </a>`, ''); 
+}
+
+const list = createItem(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', list);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
