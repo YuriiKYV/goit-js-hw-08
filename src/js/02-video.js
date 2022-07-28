eb8b53cec5594940c27d497a60e2b3bd638f722a
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-07-28 22:39:42 +0300

    Finish tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const dataObject = {};
+
+const form = document.querySelector('.feedback-form');
+
+chekKey();
+
+form.addEventListener('input', throttle(addKey, 500));
+form.addEventListener('submit', submit);
+
+function addKey() {
+    dataObject.email = form.elements.email.value;
+    dataObject.message = form.elements.message.value;
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
+};
+
+
+
+function submit(event) {
+    event.preventDefault();
+    console.log(dataObject.email);
+    console.log(dataObject.message);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset();
+    
+};
+
+function chekKey() {
+    const dataParse = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (!dataParse === false) {
+        if (dataParse.email) {
+            form.elements.email.value = dataParse.email;
+        }
+        if (dataParse.message) {
+            form.elements.message.value = dataParse.message;
+        }
+    }
+};
+
 
